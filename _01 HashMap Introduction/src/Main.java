b030686f29c3bf38cbb72cb4ad5a8bbe9b3733fe
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-01-02 00:08:11 +0530

    maps

--- _01 HashMap Introduction/src/Main.java
+++ _01 HashMap Introduction/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -52,7 +53,7 @@ public class Main {
         // -----------------------------------------
         // Search Key
         // --------------------------------
-        // 5. containsKey(Object key) :
+        // 5.1. containsKey(Object key) :
         // Checks if the map contains the specified key.
         // returns true/false
         System.out.println("--------------------------------");
@@ -62,7 +63,7 @@ public class Main {
 
         // Search value
         // --------------------------------
-        // 6. containsValue(Object value)
+        // 5.2. containsValue(Object value)
         // Checks if the map contains one or more keys that map to the specified value.
         // returns true/false
         System.out.println("Contains 400? " + map.containsValue(400));
@@ -70,7 +71,7 @@ public class Main {
 
         // Search emptiness
         // --------------------------------
-        // 7. isEmpty()
+        // 5.3. isEmpty()
         // Checks if the map is empty.
         // returns true/false
         System.out.println("is map empty ? " + map.isEmpty());
@@ -78,11 +79,45 @@ public class Main {
 
 
         // Iteration Operations :
-        // -----------------------------------------
-        // Search Key
         // --------------------------------
+        // Iterate Keys
+        // --------------------------------
+        // 6.1. keySet() :
+        // Returns a Set view of all the keys in the map.
+        System.out.println("--------------------------------");
+        System.out.println("Print HashMap (Iteration) -");
+        System.out.println("map.keySet() : " + map.keySet());
+        for (String key : map.keySet()) {
+            System.out.println(key + ": " + map.get(key));
+        }
+
+        // Iterate Values
+        // --------------------------------
+        // 6.2. values() :
+        // Returns a Collection view of all the values in the map.
+        System.out.println("map.values() : " + map.values());
+        for (int value : map.values()) {
+            System.out.println( value);
+        }
+
+        // Iterate Key-value pairs
+        // --------------------------------
+        // 6.3. entrySet() :
+        // Returns a Set view of all the key-value pairs (Map.Entry<K, V> objects) in the map.
+        System.out.println("Print HashMap (Iteration) -");
+        System.out.println("map.entrySet() : " + map.entrySet());
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry );
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+        // A. getKey(): Retrieves the key from the current entry.
+        // B. getValue(): Retrieves the value associated with the current key in the entry.
 
 
 
+        // 7. size() :
+        // Returns the number of key-value pairs in the map.
+        System.out.println("--------------------------------");
+        System.out.println("Size Of map map.size() :- "+ map.size());
     }
 }
