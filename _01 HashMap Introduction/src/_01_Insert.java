f4399813bb0ec54198ea3c191bc6b839c609d3cf
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-01-02 19:09:03 +0530

    maps

--- _01 HashMap Introduction/src/_01_Insert.java
+++ _01 HashMap Introduction/src/_01_Insert.java
@@ -1,137 +1,54 @@
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
-public class Main2 {
+public class _01_Insert {
     public static void main(String[] args) {
-        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> map1 = new HashMap<>();
 
-        // Adding elements
-        // 1. put(K key, V value) :
-        // Inserts a key-value pair into the map or updates the value if the key already exists.
-        map.put("Apple", 300);
-        map.put("Banana", 35);
-        map.put("Orange", 70);
-        System.out.println("map -> "+ map);
-
-        // Accessing elements
-        // 2. get(Object key) :
-        // Returns the value associated with the specified key, or null if the key is not found.
-        // returns value/null
-        System.out.println("Apple Price: " + map.get("Apple"));
-        System.out.println("Apple2 Price: " + map.get("Apple2"));
+        // Adding elements :
+        // -------------------------------------------------------------
+        // Note: Every item is inserted in random positions in the HashMap.
 
 
-        // Iterating over the map
+        // 1.A. put(K key, V value) :
+        //      Inserts a key-value pair into the map or updates the value if the key already exists.
+        // -----------------------------------------------------------------------------------------
+        // put() Return Value -
+        // ---------------------
+        // a. if key is already associated with any value, returns the previously associated value
+        // b. if key is not associated with any value, returns null
+        // Note : If key is previously associated with a null value, then also the method returns null
+        map1.put("Apple", 300);
+        map1.put("Banana", 35);
+        System.out.println(map1.put("Orange", 70));  // return null  : as Orange key does't exist
+        System.out.println(map1.put("Orange", 90));  // return 70    : as Orange key exists with value 70
+        System.out.println("map1 -> "+ map1);
         System.out.println("--------------------------------");
-        System.out.println("Print HashMap -");
-        for (String key : map.keySet()) {
-            System.out.println(key + ": " + map.get(key));
-        }
 
-        // Removing an element
-        // 3. remove(Object key) :
-        // Removes the mapping for the specified key if present.
+
+        // 1.B. putAll(Map<? extends K, ? extends V> m) :
+        //      map.putAll(anotherMap);
+        //      Copies all key-value pairs from another map into this map.
+        // ------------------------------------------------------------------------------------------
+        // The putAll() method does not return any values.
+        // ----------------------------------------------
+        // Q. When Does Overriding Happen?
+        // A. If a key in the source map matches a key in the current map, the value for that key in the current map will be updated to the value from the source map.
+        HashMap<String, Integer> map2 = new HashMap<>();
+        map2.put("Apple", 30000);
+        map2.put("Kiwi", 77);
+        map2.put("Strawberry", 900);
+        System.out.println("map2 -> "+ map2);
         System.out.println("--------------------------------");
-        map.remove("Orange");
-        System.out.println("Print HashMap (After Removing Orange Key) -");
-        for (String key : map.keySet()) {
-            System.out.println(key + ": " + map.get(key));
-        }
 
-
-        // Update Existing Key value
-        // 4. put(K key, V value) :
-        // Inserts a key-value pair into the map or updates the value if the key already exists.
-        System.out.println("--------------------------------");
-        map.put("Apple", 400);  // Update value of "Apple"
-        System.out.println("Print HashMap (After Updating Apple value) -");
-        for (String key : map.keySet()) {
-            System.out.println(key + ": " + map.get(key));
-        }
-
-
-        // Search Operations :
-        // -----------------------------------------
-        // Search Key
-        // --------------------------------
-        // 5.1. containsKey(Object key) :
-        // Checks if the map contains the specified key.
-        // returns true/false
-        System.out.println("--------------------------------");
-        System.out.println("Print HashMap (Search Operations) -");
-        System.out.println("Contains Banana? " + map.containsKey("Banana"));
-        System.out.println("Contains Kiwi? " + map.containsKey("Kiwi"));
-
-        // Search value
-        // --------------------------------
-        // 5.2. containsValue(Object value)
-        // Checks if the map contains one or more keys that map to the specified value.
-        // returns true/false
-        System.out.println("Contains 400? " + map.containsValue(400));
-        System.out.println("Contains 80000? " + map.containsValue(8000));
-
-        // Search emptiness
-        // --------------------------------
-        // 5.3. isEmpty()
-        // Checks if the map is empty.
-        // returns true/false
-        System.out.println("is map empty ? " + map.isEmpty());
+        map1.putAll(map2);
+        // Apple key already existing in map1 so override it from map2.
+        // other keys are not existing in map1 so add them as it is.
+        System.out.println("Print HashMap map1 after merging with map2 -");
+        System.out.println("map1 -> "+ map1);
 
 
 
-        // Iteration Operations :
-        // --------------------------------
-        // Iterate Keys
-        // --------------------------------
-        // 6.1. keySet() :
-        // Returns a Set view of all the keys in the map.
-        System.out.println("--------------------------------");
-        System.out.println("Print HashMap (Iteration) -");
-        System.out.println("map.keySet() : " + map.keySet());
-        // or Set<String> keys = map.keySet();
-        for (String key : map.keySet()) {
-            System.out.println(key + ": " + map.get(key));
-        }
-
-        // Iterate Values
-        // --------------------------------
-        // 6.2. values() :
-        // Returns a Collection view of all the values in the map.
-        System.out.println("map.values() : " + map.values());
-        // or Collection<Integer> values = map.values();
-        for (int value : map.values()) {
-            System.out.println( value);
-        }
-
-        // Iterate Key-value pairs
-        // --------------------------------
-        // 6.3. entrySet() :
-        // Returns a Set view of all the key-value pairs (Map.Entry<K, V> objects) in the map.
-        System.out.println("Print HashMap (Iteration) -");
-        System.out.println("map.entrySet() : " + map.entrySet());
-        // or Set<Map.Entry<String, Integer>> entries = map.entrySet();
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.println(entry );
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
-        // A. getKey(): Retrieves the key from the current entry.
-        // B. getValue(): Retrieves the value associated with the current key in the entry.
-
-
-
-        // 7. size() :
-        // Returns the number of key-value pairs in the map.
-        System.out.println("--------------------------------");
-        System.out.println("Size Of map map.size() :- "+ map.size());
-
-
-        // 8. clear() :
-        // Removes all key-value pairs from the map.
-        System.out.println("--------------------------------");
-        map.clear();
-        System.out.println("Size Of map After clear():- "+ map.size());
     }
 }
 
