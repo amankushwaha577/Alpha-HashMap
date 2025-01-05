6c2b85afdabb3965440302c24a5e3ea7b0845b93
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-01-05 15:58:23 +0530

    hash

---
+++ _00 Hashing/src/FrequencyCounter.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class FrequencyCounter {
+
+    public static void main(String[] args) {
+        // Dummy input for the number of elements and elements array
+        int n = 5; // 5 elements
+        int[] arr = {1, 2, 3, 1, 2};
+
+        // Initialize a hash array to store the frequency of numbers from 0 to 9
+        int[] hash = new int[10]; // Since the range is 0 to 9, size is 10
+        // hash =[0,0,0,0,0,0,0,0,0]
+
+        // Precompute the frequency of each element in the array
+        for (int i = 0; i < n; i++) {
+            if (arr[i] >= 0 && arr[i] < hash.length) {
+                // Ensure the number is within the hash array range
+                // because it can't mesaure <0 like -2,-1  & >9 like 10,11,12
+                hash[arr[i]] += 1;
+            }
+        }
+
+        // Dummy input for queries and results
+        int[] queries = {1, 2, 3, 4};
+
+        // Answer each query by printing the frequency of the given number
+        for (int number : queries) {
+            if (number >= 0 && number <= 9) {
+                System.out.println("Frequency of " + number + ": " + hash[number]);
+            } else {
+                System.out.println("Number out of range!");
+            }
+        }
+    }
+}
