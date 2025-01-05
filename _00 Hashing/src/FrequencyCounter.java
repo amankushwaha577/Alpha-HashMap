bdbbebfa8fa274a1dfba2b762389061e12519f5f
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-01-05 16:06:59 +0530

    hash

--- _00 Hashing/src/FrequencyCounter.java
+++ _00 Hashing/src/FrequencyCounter.java
@@ -13,10 +13,14 @@ public class FrequencyCounter {
 
         // Precompute the frequency of each element in the array
         for (int i = 0; i < n; i++) {
-            if (arr[i] >= 0 && arr[i] < hash.length) {
+            if (arr[i] >= 0 && arr[i] <= 9) {
                 // Ensure the number is within the hash array range
                 // because it can't mesaure <0 like -2,-1  & >9 like 10,11,12
+
                 hash[arr[i]] += 1;
+                // arr[i] gives the value of the current element in the array.
+                // hash[arr[i]] accesses the index in the hash array corresponding to the value of arr[i]
+                // The += 1 operation increases the count at that index by 1.
             }
         }
 
